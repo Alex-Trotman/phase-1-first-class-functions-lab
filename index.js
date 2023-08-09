1d879da6a20c1f6f7500f7497f6160eb0ec708e8
Alex <alextrotman04@gmail.com>
2023-08-09 16:47:24 -0500

    passes all tests

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const returnFirstTwoDrivers = ((array) => [array[0], array[1]])
-const returnLastTwoDrivers = ((array) => [array[2], array[3]])
+const returnFirstTwoDrivers = ((array) => [array[0], array[1]]);
+const returnLastTwoDrivers = ((array) => [array[2], array[3]]);
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(int){
+    return function(fair){
+        return (int * fair)
+    }
+}
+const fareDoubler = createFareMultiplier(2) 
+const fareTripler = createFareMultiplier(3) 
+function selectDifferentDrivers(array, param2){
+    return param2(array);
+}
